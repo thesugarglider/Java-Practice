9562134f132d99f527e9c6844bdb8afc166f33f3
The Sugar Glider <thesugarglider7@gmail.com>
2022-08-18 17:43:38 +0530

    Book Runner Updated

--- src/com/sugarglider/book/BookRunner.java
+++ src/com/sugarglider/book/BookRunner.java
@@ -2,6 +2,10 @@ package com.sugarglider.book;
 
 public class BookRunner {
     public static void main(String[] args) {
+        Book artOfComputerProgramming = new Book();
 
+        artOfComputerProgramming.setNoOfCopies(5);
+        System.out.println(artOfComputerProgramming.getNoOfCopies());
+        artOfComputerProgramming.openBook();
     }
 }
