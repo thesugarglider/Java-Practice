c3d9b8016c6db35844bbfe119062d86398e5a4a8
The Sugar Glider <thesugarglider7@gmail.com>
2022-08-26 13:09:15 +0530

    modified MyCharRunner class.

--- src/com/sugarglider/datatypes/character/MyCharRunner.java
+++ src/com/sugarglider/datatypes/character/MyCharRunner.java
@@ -3,9 +3,20 @@ package com.sugarglider.datatypes.character;
 public class MyCharRunner {
     public static void main(String[] args) {
         MyChar myChar = new MyChar('a');
-        System.out.println(myChar.isVowel());
-        System.out.println(myChar.isDigit());
-        System.out.println(myChar.isAlphabet());
-        System.out.println(myChar.isConsonant());
+        if(myChar.isVowel()){
+            System.out.println("It is vowel");
+        }
+
+        if(myChar.isDigit()){
+            System.out.println("It is digit");
+        }
+        if(myChar.isAlphabet()){
+            System.out.println("It is alphabet");
+        }
+
+        if(myChar.isConsonant()){
+            System.out.println("It is consonant");
+        }
+
     }
 }
