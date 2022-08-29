ac1a8bc78eb3c2d99fc3e8f030967c055d3be055
pravinkumarosingh <headfryer@gmail.com>
2022-08-29 15:56:26 +0530

    loops

---
+++ src/com/sugarglider/loops/forloop/ForLoop.java
@@ -0,0 +1,9 @@
+package com.sugarglider.loops.forloop;
+
+public class ForLoop {
+    public static void main(String[] args) {
+        for (int i=0;i<=10;i=i+2){
+            System.out.print(i+" ");
+        }
+    }
+}
