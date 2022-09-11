7e5de27a5d6b5b0458939f04810211a28178d8eb
Pravinkumar Singh <pravinkumarosingh@gmail.com>
2022-09-11 16:19:25 +0530

    do while loop exercise

--- src/com/sugarglider/loops/doowhileloop/DoWhileLoop.java
+++ src/com/sugarglider/loops/doowhileloop/DoWhileLoop.java
@@ -1,4 +1,16 @@
 package com.sugarglider.loops.doowhileloop;
 
+import java.util.Scanner;
+
 public class DoWhileLoop {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number;
+        do {
+            System.out.print("Enter a number: ");
+            number = scanner.nextInt();
+            System.out.println("Cube is: "+(number*number*number));
+        }while (number>0);
+        System.out.println("Thank You Have A Fun");
+    }
 }
