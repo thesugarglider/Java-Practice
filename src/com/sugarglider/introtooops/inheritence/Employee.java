e813dcc517076f04e2f9867c5db05178ed04ab4f
Pravinkumar Singh <pravinkumarosingh@gmail.com>
2022-10-02 23:36:26 +0530

    inheritence example modified.

---
+++ src/com/sugarglider/introtooops/inheritence/Employee.java
@@ -0,0 +1,45 @@
+package com.sugarglider.introtooops.inheritence;
+
+public class Employee extends Person {
+    private String title;
+    private String employerName;
+    private char employerGrade;
+    private int salary;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getEmployerName() {
+        return employerName;
+    }
+
+    public void setEmployerName(String employerName) {
+        this.employerName = employerName;
+    }
+
+    public char getEmployerGrade() {
+        return employerGrade;
+    }
+
+    public void setEmployerGrade(char employerGrade) {
+        this.employerGrade = employerGrade;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public String toString(){
+        return String.format("Name [%s], Email [%s], Phone [%d], Grade [%c]",super.getName(),super.getEmail(),super.getNumber(),employerGrade);
+    }
+
+}
