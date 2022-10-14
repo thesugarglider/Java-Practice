38000d31ba953dfa4bd512af257be6df3f269ad8
pravinkumarosingh <headfryer@gmail.com>
2022-10-14 10:21:43 +0530

    collections added and list examples.

---
+++ src/com/sugarglider/collections/ListExample4.java
@@ -0,0 +1,27 @@
+package com.sugarglider.collections;
+
+import java.util.Iterator;
+import java.util.List;
+
+public class ListExample4 {
+    public static void main(String[] args) {
+        List<String> animals = List.of("Yak","Zebra","Cheetah","Leopard");
+
+        for(int i=0;i<animals.size();i++){
+            System.out.println(animals.get(i));
+        }
+
+        System.out.println();
+
+        for (String animal:animals){
+            System.out.println(animal);
+        }
+
+        System.out.println();
+
+        Iterator animalIterator = animals.iterator();
+        while (animalIterator.hasNext()){
+            System.out.println(animalIterator.next());
+        }
+    }
+}
