ceae00b04d0a993ff564a1ae86ee9cddf60aaf84
Pravinkumar Singh <pravinkumarosingh@gmail.com>
2022-12-03 11:21:59 +0530

    join method for communication between threads

--- src/com/sugarglider/threadsandconcurrency/Example3.java
+++ src/com/sugarglider/threadsandconcurrency/Example3.java
@@ -1,25 +1,22 @@
 package com.sugarglider.threadsandconcurrency;
 
-public class Example2MainClass {
-    public static void main(String[] args) {
-
-
-        System.out.println("Task 1 Started");
-        for (int j=101;j<=105;j++) {
-            System.out.println(j+" ");
-        }
-        System.out.println("Task 1 Done");
+public class Example3 {
+    public static void main(String[] args) throws InterruptedException {
+        Example1 task1 = new Example1();
+        task1.setPriority(10);
+        task1.run();
 
         Example2 task2 = new Example2();
         Thread task2Thread = new Thread(task2);
-        task2Thread.setPriority(10);
         task2Thread.start();
+
+        task1.join();
+        task2Thread.join();
 
         System.out.println("Task 3 Started");
         for (int k=301;k<=305;k++) {
             System.out.println(k+" ");
         }
         System.out.println("Task 3 Done");
-
     }
 }
