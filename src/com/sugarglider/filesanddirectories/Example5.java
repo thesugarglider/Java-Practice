937938e0b93222f5ca2a4557762686dc2cab8c4b
pravinkumarosingh <headfryer@gmail.com>
2023-01-01 11:26:01 +0530

    writing to a file example.

---
+++ src/com/sugarglider/filesanddirectories/Example5.java
@@ -0,0 +1,17 @@
+package com.sugarglider.filesanddirectories;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class Example5 {
+    public static void main(String[] args) throws IOException {
+        Path pathToWriteFile = Paths.get("./resources/filetowrite.txt");
+
+        List<String> list = List.of("Apple", "Cat", "Dog");
+
+        Files.write(pathToWriteFile,list);
+    }
+}
