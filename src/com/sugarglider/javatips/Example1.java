e691d852224ae824afc2c7202dcf1b1f03f35054
pravinkumarosingh <headfryer@gmail.com>
2023-01-14 12:27:09 +0530

    imports and static imports

---
+++ src/com/sugarglider/javatips/Example1.java
@@ -0,0 +1,10 @@
+package com.sugarglider.javatips;
+
+import java.math.BigDecimal;
+import static java.lang.System.out;
+public class Example1 {
+    public static void main(String[] args) {
+        BigDecimal db = null;
+        out.println("static import");
+    }
+}
