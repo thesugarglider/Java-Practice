aa331af8c1d2bffd1cd7b437648cd246706e7d5a
pravinkumarosingh <headfryer@gmail.com>
2023-01-24 19:06:18 +0530

    static variables examples added.

---
+++ src/com/sugarglider/javatips/Example8Runner.java
@@ -0,0 +1,11 @@
+package com.sugarglider.javatips;
+
+public class Example8Runner {
+    public static void main(String[] args) {
+        Example8 player1 = new Example8("Ronaldo");
+        System.out.println(Example8.getCount());
+
+        Example8 player2 = new Example8("Messi");
+        System.out.println(Example8.getCount());
+    }
+}
