2cb85217ed2ff53037cc1982799e7e6ca506369d
pravinkumarosingh <headfryer@gmail.com>
2023-01-24 08:21:44 +0530

    access modifiers examples added.

---
+++ src/com/sugarglider/javatips/Example3_1.java
@@ -0,0 +1,7 @@
+package com.sugarglider.javatips;
+
+ class Example3_1 {
+     public static void main(String[] args) {
+         System.out.println("Default Class");
+     }
+}
---
+++ src/com/sugarglider/javatips/Example3_2.java
@@ -0,0 +1,7 @@
+package com.sugarglider.javatips;
+
+public class Example3_2 {
+    public static void main(String[] args) {
+        Example3_1 example3_1 = new Example3_1();
+    }
+}
