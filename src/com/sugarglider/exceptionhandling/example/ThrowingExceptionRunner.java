6622326819a66810f01e42204074884455967117
pravinkumarosingh <pravinkumarosingh@gmail.com>
2023-02-23 11:07:46 +0530

    modified exception method signature.

--- src/com/sugarglider/exceptionhandling/example/ThrowingExceptionRunner.java
+++ src/com/sugarglider/exceptionhandling/example/ThrowingExceptionRunner.java
@@ -1,7 +1,7 @@
 package com.sugarglider.exceptionhandling.example;
 
 public class ThrowingExceptionRunner {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws CurrenciesDoNotMatchException {
         Amount amount1 = new Amount("USD",10);
         Amount amount2 = new Amount("EUR",20);
 
