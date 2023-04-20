d11615f6b42cc001b9ab8ce4ff24c3b238529664
pravinkumarosingh <pravinkumarosingh@gmail.com>
2023-04-20 11:14:54 +0530

    new method added multiplyNumbers

--- src/com/sugarglider/functionalprogramming/Example3.java
+++ src/com/sugarglider/functionalprogramming/Example3.java
@@ -6,9 +6,14 @@ public class Example3 {
     public static void main(String[] args) {
         List<Integer> numbers = List.of(4,6,8,13,3,15);
         addNumbers(numbers);
+        multiplyNumbers(numbers);
     }
 
     public static void addNumbers(List<Integer> numbers){
         System.out.println((int) numbers.stream().reduce(0,(number1, number2) -> number1 + number2));
+    }
+
+    public static void multiplyNumbers(List<Integer> numbers){
+        System.out.println((int) numbers.stream().reduce(1, (number1, number2) -> number1 * number2));
     }
 }
