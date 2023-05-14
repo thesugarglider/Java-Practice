f2ae4aaebf169658c03954bc79ae7feb56a2ec31
pravinkumarosingh <pravinkumarosingh@gmail.com>
2023-05-14 15:42:10 +0530

    method reference example

---
+++ src/com/sugarglider/functionalprogramming/Example20Reference.java
@@ -0,0 +1,11 @@
+package com.sugarglider.functionalprogramming;
+
+public class Example20Reference {
+    public static void print(Integer integer){
+        System.out.println(integer);
+    }
+
+    public static boolean isEven(Integer number){
+        return number%2==0;
+    }
+}
