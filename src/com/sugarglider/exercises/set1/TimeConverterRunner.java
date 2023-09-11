307ce4d687114435e78b27ffdf2c35026987e0cc
pravinkumarosingh <pravinkumarosingh@gmail.com>
2023-09-11 22:15:50 +0530

    TimeConverter exercise

---
+++ src/com/sugarglider/exercises/set1/TimeConverterRunner.java
@@ -0,0 +1,8 @@
+package com.sugarglider.exercises.set1;
+
+public class TimeConverterRunner {
+    public static void main(String[] args) {
+        System.out.println("There are "+TimeConverter.convertHoursToMinutes(2)+ " minutes");
+        System.out.println("There are "+TimeConverter.convertDaysToMinutes(1)+ " minutes");
+    }
+}
