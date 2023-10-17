e9a248bdebd446c119dd338f6ae51b7d62fcd801
pravinkumarosingh <pravinkumarosingh@gmail.com>
2023-10-17 20:16:59 +0530

    data types examples added

---
+++ src/com/sugarglider/datatypes/exercises/AreaOfCircle.java
@@ -0,0 +1,14 @@
+package com.sugarglider.datatypes.exercises;
+
+public class AreaOfCircle {
+    public static void main(String[] args) {
+        double radius, area;
+
+        radius = 6;
+
+        area = Math.PI * radius * radius;
+
+        System.out.println("Area of the circle is "+ area);
+
+    }
+}
---
+++ src/com/sugarglider/datatypes/exercises/CharacterDemo.java
@@ -0,0 +1,15 @@
+package com.sugarglider.datatypes.exercises;
+
+public class CharacterDemo {
+    public static void main(String[] args) {
+        char ch;
+        ch = 'X';
+        System.out.println("Character ch contains "+ ch);
+
+        ch++;
+        System.out.println("Character ch is now "+ch);
+
+        ch = 90;
+        System.out.println("Character ch now becomes "+ch);
+    }
+}
