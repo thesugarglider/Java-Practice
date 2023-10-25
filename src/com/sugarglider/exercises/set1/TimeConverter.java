28f67b5e2210a7021f942ec9147dcdeb28469100
pravinkumarosingh <pravinkumarosingh@gmail.com>
2023-10-25 09:42:50 +0530

    Time converter modified for negative values

--- src/com/sugarglider/exercises/set1/TimeConverter.java
+++ src/com/sugarglider/exercises/set1/TimeConverter.java
@@ -5,10 +5,16 @@ public class TimeConverter {
     static final int MINUTES = 60;
     static final int DAY = 24;
     public static int convertHoursToMinutes(int hours){
+        if (hours < 0){
+            return -1;
+        }
         return hours * MINUTES;
     }
 
     public static int convertDaysToMinutes(int days){
+        if (days < 0){
+            return -1;
+        }
         return days * DAY * MINUTES;
     }
 }
