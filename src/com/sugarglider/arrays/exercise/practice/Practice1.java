c7a8ee8ca1dc6a565e4e93832fb94a5357fafaeb
pravinkumarosingh <pravinkumarosingh@gmail.com>
2024-02-17 11:49:07 +0530

    arrays practice

--- src/com/sugarglider/arrays/exercise/practice/Practice1.java
+++ src/com/sugarglider/arrays/exercise/practice/Practice1.java
@@ -23,5 +23,11 @@ public class Practice1 {
             System.out.println("Middle element is " + firstTen[array.length/2]);
         }
 
+        int newArray[];
+        newArray = new int[]{5,4,3,2,1};
+        for(int element : newArray){
+            System.out.println(element);
+        }
+
     }
 }
