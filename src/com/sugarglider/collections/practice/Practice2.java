8536622392d931924f8a91b57f7564a4d028108f
pravinkumarosingh <pravinkumarosingh@gmail.com>
2024-02-24 12:03:48 +0530

    more methods and practice on ArrayList

--- src/com/sugarglider/collections/practice/Practice2.java
+++ src/com/sugarglider/collections/practice/Practice2.java
@@ -1,6 +1,8 @@
 package com.sugarglider.collections.practice;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 
 public class Practice2 {
@@ -20,5 +22,46 @@ public class Practice2 {
                 List.of("eggs", "curd", "biscuits"));
 
         System.out.println(newList);
+
+        arrayList.addAll(newList);
+        System.out.println(arrayList);
+
+        System.out.println(newList.get(2));
+
+        if(newList.contains("curd")){
+            System.out.println("New List Contains Curd");
+        }
+
+        System.out.println(arrayList.indexOf("eggs"));
+        System.out.println(arrayList.lastIndexOf("eggs"));
+
+        arrayList.remove(4);
+        System.out.println(arrayList);
+        arrayList.remove("eggs");
+        System.out.println(arrayList);
+
+        arrayList.removeAll(List.of("biscuits", "bananas"));
+        System.out.println(arrayList);
+
+        arrayList.retainAll(List.of("chicken", "bananas","apple"));
+        System.out.println(arrayList);
+
+        arrayList.clear();
+        System.out.println("Food list -> " + arrayList);
+
+        arrayList.addAll(List.of("apple", "almond", "fig", "chicken"));
+        arrayList.addAll(Arrays.asList("eggs", "oranges", "foxnut"));
+
+        System.out.println("Food List -> " + arrayList);
+
+        arrayList.sort(Comparator.naturalOrder());
+        System.out.println("Food List -> " + arrayList);
+
+        arrayList.sort(Comparator.reverseOrder());
+        System.out.println("Food List -> " + arrayList);
+
+        String[] groceryArray = arrayList.toArray(new String[arrayList.size()]);
+        System.out.println(Arrays.toString(groceryArray));
+
     }
 }
